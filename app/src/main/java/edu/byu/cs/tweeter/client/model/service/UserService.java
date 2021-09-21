807c7757f2d91f456278de91eaebafaee22f20dd
Maddie Petty <maddiepetty@maddies-mbp.lan>
2021-09-20 19:37:53 -0600

    Login complete

--- app/src/main/java/edu/byu/cs/tweeter/client/model/service/UserService.java
+++ app/src/main/java/edu/byu/cs/tweeter/client/model/service/UserService.java
@@ -9,6 +9,8 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 import edu.byu.cs.tweeter.client.backgroundTask.GetUserTask;
+import edu.byu.cs.tweeter.client.backgroundTask.LoginTask;
+import edu.byu.cs.tweeter.client.cache.Cache;
 import edu.byu.cs.tweeter.model.domain.AuthToken;
 import edu.byu.cs.tweeter.model.domain.User;
 
@@ -24,7 +26,6 @@ public class UserService {
         GetUserTask getUserTask = new GetUserTask(authtoken, alias, new GetUserHandler(observer));
         ExecutorService executor = Executors.newSingleThreadExecutor();
         executor.execute(getUserTask);
-
     }
 
     /**
@@ -48,6 +49,52 @@ public class UserService {
             } else if (msg.getData().containsKey(GetUserTask.EXCEPTION_KEY)) {
                 Exception ex = (Exception) msg.getData().getSerializable(GetUserTask.EXCEPTION_KEY);
                 observer.getUserThrewException(ex);
+            }
+        }
+    }
+
+
+    public interface LoginObserver {
+        void loginSucceeded(AuthToken authToken, User user);
+        void loginFailed(String message);
+        void loginThrewException(Exception e);
+    }
+
+    public void login(String alias, String password, LoginObserver observer) {
+        // Send the login request.
+        LoginTask loginTask = new LoginTask(alias, password, new LoginHandler(observer));
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+        executor.execute(loginTask);
+    }
+
+    /**
+     * Message handler (i.e., observer) for LoginTask
+     */
+    private class LoginHandler extends Handler {
+        private LoginObserver observer;
+
+        public LoginHandler(LoginObserver observer) {
+            this.observer = observer;
+        }
+
+        @Override
+        public void handleMessage(@NonNull Message msg) {
+            boolean success = msg.getData().getBoolean(LoginTask.SUCCESS_KEY);
+            if (success) {
+                User loggedInUser = (User) msg.getData().getSerializable(LoginTask.USER_KEY);
+                AuthToken authToken = (AuthToken) msg.getData().getSerializable(LoginTask.AUTH_TOKEN_KEY);
+
+                // Cache user session information
+                Cache.getInstance().setCurrUser(loggedInUser);
+                Cache.getInstance().setCurrUserAuthToken(authToken);
+
+                observer.loginSucceeded(authToken, loggedInUser);
+            } else if (msg.getData().containsKey(LoginTask.MESSAGE_KEY)) {
+                String message = msg.getData().getString(LoginTask.MESSAGE_KEY);
+                observer.loginFailed(message);
+            } else if (msg.getData().containsKey(LoginTask.EXCEPTION_KEY)) {
+                Exception ex = (Exception) msg.getData().getSerializable(LoginTask.EXCEPTION_KEY);
+                observer.loginThrewException(ex);
             }
         }
     }
---
+++ app/src/main/java/edu/byu/cs/tweeter/client/presenter/LoginPresenter.java
@@ -0,0 +1,68 @@
+package edu.byu.cs.tweeter.client.presenter;
+
+import edu.byu.cs.tweeter.client.model.service.UserService;
+import edu.byu.cs.tweeter.model.domain.AuthToken;
+import edu.byu.cs.tweeter.model.domain.User;
+
+public class LoginPresenter implements UserService.LoginObserver {
+    public interface View {
+        void navigateToUser(User user);
+
+        void displayErrorMessage(String message);
+        void clearErrorMessage();
+
+        void displayInfoMessage(String message);
+        void clearInfoMessage();
+    }
+
+    private View view;
+
+    public LoginPresenter(View view) {
+        this.view = view;
+    }
+
+    public void login(String alias, String password) {
+        view.clearErrorMessage();
+        view.clearInfoMessage();
+
+        String message = validateLogin(alias, password);
+
+        if (message == null) {
+            view.displayInfoMessage("Logging In...");
+            new UserService().login(alias, password, this);
+        }
+        else {
+            view.displayErrorMessage("Login failed: " + message);
+        }
+    }
+
+    private String validateLogin(String alias, String password) {
+        if (alias.charAt(0) != '@') {
+            return "Alias must begin with @.";
+        }
+        if (alias.length() < 2) {
+            return "Alias must contain 1 or more characters after the @.";
+        }
+        if (password.length() == 0) {
+            return "Password cannot be empty.";
+        }
+        return null;
+    }
+
+    @Override
+    public void loginSucceeded(AuthToken authToken, User user) {
+        view.navigateToUser(user);
+        view.clearErrorMessage();
+        view.displayInfoMessage("Hello " + user.getName());
+    }
+
+    @Override
+    public void loginFailed(String message) {
+        view.displayErrorMessage("Login failed: " + message);
+    }
+
+    @Override
+    public void loginThrewException(Exception e) {
+        view.displayErrorMessage("Login failed: " + e.getMessage());
+    }
+}
